1e1fd0101c7421a70decfb9c4e6b50ad592c083f
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-06-10 15:39:01 +0700

    if while

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -24,5 +24,14 @@ int main () {
     ifstream infile;
     infile.open(NamaFile, ios::in);
     cout << endl << ">= Membuka dan membaca file " << endl;
-    
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << '\n';
+        }
+        infile.close();
+    }
+    else cout << "Unable to open file";
+    return 0;
 }
