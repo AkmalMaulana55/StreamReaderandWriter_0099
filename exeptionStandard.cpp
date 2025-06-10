5b6b7418019b6326818d4461a0197d66844bd812
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-06-10 15:10:38 +0700

    penangkap menggunakan obyek exception

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -8,7 +8,9 @@ int main()
     cout << "Awal Program" << endl;
     try {
         array<int, 3> data = { 10, 20, 30 };
-
         cout<<data.at(5)<<endl;
+    }
+    catch (exception& e) {
+        cout << e.what() << endl;
     }
 }
