5809e82a977c421c1330f541e0c5e1e126976dbe
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-06-10 15:22:32 +0700

    catch ...

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -11,5 +11,9 @@ int main()
     catch (int a) {
         cout << "Pengecualian akan dieksekusi" << endl;
     }
-    
+    catch (...) {
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
 }
