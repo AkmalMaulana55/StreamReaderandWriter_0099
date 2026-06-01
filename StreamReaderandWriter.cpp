8aae902323b70a55cad7027adeaba2178ccf0146
niel55-stack <vylingsniel55@gmail.com>
2026-06-01 19:09:09 +0700

    membuat kondisi jika file tidak ada

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -59,4 +59,10 @@ int main()
         // menutup file jika telah selesai
         infile.close();
     }
+
+    // kondisi jika file tidak ada
+    else
+        cout << "Unable to open file";
+
+    return 0;
 }
